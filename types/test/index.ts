8c958e774be211f646be729296c54fdf432c744f
Felix Bernhard <mail@felix-bernhard.com>
2025-05-22 20:27:42 +0200

    (plugin) add types test descriptions

--- types/test/index.ts
+++ types/test/index.ts
@@ -6,7 +6,10 @@ const app = createApp({
   template: `<vue-modal name="awesome-modal"></vue-modal>`
 });
 
+// 1. Install the plugin without any options
 app.use(VueJSModal);
+
+// 2. Install the plugin with custom options
 app.use<VueJSModalOptions>(VueJSModal, {
   componentName: "another-modal-name",
   dialog: false
@@ -14,20 +17,24 @@ app.use<VueJSModalOptions>(VueJSModal, {
 
 const vm = app.mount("#app");
 
+// 3. Call injected modal methods
 vm.$modal.show("awesome-modal");
 vm.$modal.hide("awesome-modal", { customeEvent: "customEventParam" });
 vm.$modal.toggle("awesome-modal");
 
+// 4. Show an SFC
 vm.$modal.show(TestComponent, {
   text: 'Lorem ipsum'
 })
 
+// 5. Show an async SFC
 const AsyncTestComponent = defineAsyncComponent(() => import('./TestComponent'));
 
 vm.$modal.show(AsyncTestComponent, {
   text: 'Lorem ipsum'
 })
 
+// 6. Show a runtime component
 vm.$modal.show(
   {
     template: `<p>{{ text }}</p> `,
