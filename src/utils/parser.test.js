9fb19bdfe9ce4654a855cb63dee5f42dfd0f20c7
Felix Bernhard <mail@felix-bernhard.com>
2025-05-22 19:02:02 +0200

    (plugin) fix tests

--- src/utils/parser.test.js
+++ src/utils/parser.test.js
@@ -7,39 +7,39 @@ describe('parser.js', () => {
       it('Should parse numbers', () => {
         let object = parseNumber(10)
 
-        expect(object.value).to.be.a('number')
-        expect(object.type).to.be.a('string')
+        expect(typeof object.value).toBe('number')
+        expect(typeof object.type).toBe('string')
 
-        expect(object.value).to.equal(10)
-        expect(object.type).to.equal('px')
+        expect(object.value).toEqual(10)
+        expect(object.type).toEqual('px')
       })
 
       it('Should parse strings', () => {
         let object = parseNumber('10')
 
-        expect(object.value).to.be.a('number')
-        expect(object.type).to.be.a('string')
+        expect(typeof object.value).toBe('number')
+        expect(typeof object.type).toBe('string')
 
-        expect(object.value).to.equal(10)
-        expect(object.type).to.equal('px')
+        expect(object.value).toEqual(10)
+        expect(object.type).toEqual('px')
       })
 
       it ('Should parse "auto" string, auto => {type: "auto", value: 0}', () => {
         let object = parseNumber('auto')
 
-        expect(object.value).to.equal(0)
-        expect(object.type).to.equal('auto')
+        expect(object.value).toEqual(0)
+        expect(object.type).toEqual('auto')
       })
 
       it ('Should parse wrong types', () => {
         let nullValue = parseNumber(null)
         let booleanValue = parseNumber(false)
 
-        expect(nullValue.value).to.equal(null)
-        expect(nullValue.type).to.equal('')
+        expect(nullValue.value).toEqual(null)
+        expect(nullValue.type).toEqual('')
 
-        expect(booleanValue.value).to.equal(false)
-        expect(booleanValue.type).to.equal('')
+        expect(booleanValue.value).toEqual(false)
+        expect(booleanValue.type).toEqual('')
       })
     })
 
@@ -47,22 +47,22 @@ describe('parser.js', () => {
       it ('Should parse "px"', () => {
         let object = parseNumber('10px')
 
-        expect(object.value).to.equal(10)
-        expect(object.type).to.equal('px')
+        expect(object.value).toEqual(10)
+        expect(object.type).toEqual('px')
       })
 
       it ('Should parse "%"', () => {
         let object = parseNumber('10%')
 
-        expect(object.value).to.equal(10)
-        expect(object.type).to.equal('%')
+        expect(object.value).toEqual(10)
+        expect(object.type).toEqual('%')
       })
 
       it ('Should not parse "%px"', () => {
         let object = parseNumber('10%px')
 
-        expect(object.value).to.be.a('string')
-        expect(object.type).to.equal('')
+        expect(typeof object.value).toBe('string')
+        expect(object.type).toEqual('')
       })
     })
   })
