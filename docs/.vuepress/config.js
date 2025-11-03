bdd302f444f5aa6d676527b8c4f4ca5ba5169c3f
Felix Bernhard <mail@felix-bernhard.com>
2025-11-03 23:35:31 +0100

    (docs) add favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,13 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 import { searchPlugin } from '@vuepress/plugin-search'
 import sidebar from './sidebar'
+import head from './head'
 
 export default defineUserConfig({
   bundler: webpackBundler(),
   base: '/vue-js-modal/',
   title: 'Vue.js Modal (Vue 3)',
+  head,
   port: 8081,
   description: 'Simple, flexible, Vue.js modal plugin',
   plugins: [searchPlugin()],
---
+++ docs/.vuepress/head.js
@@ -0,0 +1,28 @@
+export default [
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'images/icons/favicon.ico'
+    }
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: 'images/icons/favicon-16x16.png'
+    }
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: 'images/icons/favicon-32x32.png'
+    }
+  ]
+]
